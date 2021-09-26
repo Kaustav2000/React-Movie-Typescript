e4c438ca61e86dd6e590426f6d657e6f86a91e90
Kaustav Bhattacharya <kbhattacharya75@gmail.com>
2021-09-26 22:18:58 +0530

    error solved

--- src/components/WhatsPopular/WhatsPopular.tsx
+++ src/components/WhatsPopular/WhatsPopular.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { Heading, Categories, Category } from "./WhatsPopular.style";
 
-const WhatsPopular = (props) => {
-
+const WhatsPopular = () => {
   return (
     <Heading>
-      <h1>{props.p}</h1>
       <Categories>
         <Category>Streaming</Category>
         <Category>On Tv</Category>
